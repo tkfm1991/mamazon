16ee26acdd57ce54abb2e70995a781c38fd288eb
tkfm1991 <kuroro1991@gmail.com>
2020-06-30 11:09:15 +0900

    lesson06

--- mamazon/urls.py
+++ mamazon/urls.py
@@ -5,5 +5,7 @@ app_name = 'mamazon'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('products/', views.ProductListView.as_view(), name='product-list')
+    path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(),
+         name='product-detail'),
 ]
